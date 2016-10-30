b3c58bd4cf9e080e0777a3d81f860206ab2afe2a
Stefan <stefan@Stefans-iMac.local>
2016-10-30 15:21:51 +0100

    iOS: Reduce the amount of stuff included in assets.zip

--- build/iOS/assets.sh
+++ build/iOS/assets.sh
@@ -7,9 +7,17 @@ fi
 
 DEST=$(mktemp -d)
 
-for dir in builtin client fonts games textures; do
+for dir in builtin client textures; do
 	cp -r ../../$dir $DEST/$dir
 done
+mkdir -p $DEST/fonts
+cp ../../fonts/*.ttf $DEST/fonts/ # PNG fonts useless with freetype
+mkdir -p $DEST/games
+if [ -d ../../games/minetest_game ]; then # copy only one subgame
+	cp -r ../../games/minetest_game $DEST/games/minetest_game
+else
+	cp -r ../../games/minimal $DEST/games/minimal
+fi
 mkdir -p $DEST/media
 cp -r deps/irrlicht/media/Shaders $DEST/media/Shaders
 
