8f71ddaf3ec70850732824ff47ddfba46d3d6803
sfan5 <sfan5@live.de>
2021-03-28 21:59:12 +0200

    .

--- builtin/async/init.lua
+++ builtin/async/init.lua
@@ -19,9 +19,8 @@ core.log("info", "Initializing Asynchronous environment")
 
 function core.job_processor(func, serialized_param)
 	local param = core.deserialize(serialized_param)
-	local retval = nil
 
-	retval = core.serialize(func(param))
+	local retval = core.serialize(func(param))
 
 	return retval or core.serialize(nil)
 end
